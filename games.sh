68e4d7f1c4a6680be40d2e115edb0e90e3bd1a1a
emili0p <emiliioizq23@gmail.com>
2026-01-16 20:20:48 -0600

    Added script to launch a steam game from the terminal

---
+++ games.sh
@@ -0,0 +1,56 @@
+#!/bin/env bash
+
+# emilio izquierdo
+# 16 de enero de 2026
+# archivo para listar todos tus juegos de steam con fzf
+# y lanzarlos desde la terminal
+# requieres fzf y steam
+
+STEAM_DIR=$(realpath "$HOME/.steam/steam" 2>/dev/null)
+if [ ! -d "$STEAM_DIR/steamapps" ]; then
+  echo "No se encontró el directorio real de Steam"
+  exit 1
+fi
+APPS_DIR="$STEAM_DIR/steamapps"
+
+command -v fzf >/dev/null || {
+  echo "fzf no esta instalado"
+  exit 1
+}
+
+command -v steam >/dev/null || {
+  echo "fzf no esta instalado"
+  exit 1
+}
+
+[ -d "$APPS_DIR" ] || {
+  echo "no se encontro el directorio de steam"
+  exit 1
+}
+
+# extraer datos
+games=$(
+  grep -R '"name"' "$APPS_DIR"/appmanifest_*.acf 2>/dev/null |
+    sed -E 's|.*/appmanifest_([0-9]+)\.acf:.*"name"[[:space:]]+"([^"]+)"|\1\t\2|' |
+    sort -k2
+)
+
+[ -z "$games" ] && {
+  echo "no se encontraron juegos"
+  exit 1
+}
+#PROMPT DE FZF
+selected=$(echo "$games" | fzf \
+  --prompt="Steam >" \
+  --with-nth=2 \
+  --delimiter=$'\t' \
+  --height=40% \
+  --border)
+
+[ -z "$selected" ] && exit 0
+
+appid=$(echo "$selected" | cut -f1)
+name=$(echo "$selected" | cut -f2)
+
+echo "lanzando : $name "
+steam "steam://rungameid/$appid" >/dev/null 2>&1 &
